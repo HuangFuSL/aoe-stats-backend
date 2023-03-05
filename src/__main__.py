73bef59fc8a5b82f526cf1f3350dc5205724f5c0
HuangFuSL <huangfusl@outlook.com>
2023-03-05 20:34:18 +0800

    Fixed failing matches

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from . import api, db
 
@@ -32,10 +33,40 @@ async def one_step():
     print('Finished')
     await db.Database().close()
 
+async def update():
+    matchIds = (await db.Database().query_finished())
+    result = await db.Database().query_players(*matchIds)
+    r = {}
+    for matchId, profileId in result:
+        r[matchId] = profileId
+    matches, records = [], []
+    for matchId, profileId in r.items():
+        if profileId is None:
+            continue
+        try:
+            (length, result), _ = await asyncio.gather(api.query_match_result(matchId, profileId), asyncio.sleep(1))
+            if length is None:
+                length = -1
+            matches.append((matchId, length))
+            records.extend([{'_' + k: v for k, v in _.items()} for _ in result])
+        except:
+            matches.append((matchId, -1))
+
+        print(len(matches), len(records))
+    if matches:
+        await db.Database().complete_matches(*matches)
+    if records:
+        await db.Database().update_players(records)
+
+    await db.Database().close()
+
 async def main():
     # Unused infinite loop
     while True:
         await asyncio.gather(one_step(), asyncio.sleep(120))
 
 if __name__ == '__main__':
-    asyncio.run(one_step())
+    if os.environ.get('ci', False):
+        asyncio.run(one_step())
+    if os.environ.get('fc', False):
+        asyncio.run(update())
