57304c05c2a508e8ef1529431d07457fff134f1f
HuangFuSL <huangfusl@outlook.com>
2023-03-06 14:59:52 +0800

    Allow reuse of database connections

--- src/db.py
+++ src/db.py
@@ -21,7 +21,13 @@ class Database():
         if not hasattr(cls, '_instance'):
             cls._instance = super(Database, cls).__new__(cls)
             cls._engine = None
+            cls._conn = None
         return cls._instance
+    
+    async def conn(self):
+        if self._conn is None:
+            self._conn = await self.engine.connect()
+        return self._conn
 
     @property
     def engine(self):
@@ -30,85 +36,87 @@ class Database():
         return self._engine
 
     async def insert_new_matches(self, data: List[Dict[str, Any]]):
-        async with self.engine.connect() as conn:
-            for row in data:
-                row['ended'] = False
-                try:
-                    await conn.execute(insert(consts.MATCH_TABLE).values(**row))
-                except IntegrityError:
-                    await conn.rollback()
-                else:
-                    await conn.commit()
+        conn = await self.conn()
+        for row in data:
+            row['ended'] = False
+            try:
+                await conn.execute(insert(consts.MATCH_TABLE).values(**row))
+            except IntegrityError:
+                await conn.rollback()
+            else:
+                await conn.commit()
 
     async def update_matches(self, *args: int):
-        async with self.engine.connect() as conn:
-            for matchId in args:
-                await conn.execute(
-                    update(consts.MATCH_TABLE) \
-                    .where(consts.MATCH_TABLE.c.matchId == matchId)
-                    .values(ended=True, length=None)
-                )
-            await conn.commit()
+        conn = await self.conn()
+        for matchId in args:
+            await conn.execute(
+                update(consts.MATCH_TABLE) \
+                .where(consts.MATCH_TABLE.c.matchId == matchId)
+                .values(ended=True, length=None)
+            )
+        await conn.commit()
 
     async def complete_matches(self, *args: Tuple[int, Optional[int]]):
-        async with self.engine.connect() as conn:
-            for matchId, length in args:
-                await conn.execute(
-                    update(consts.MATCH_TABLE) \
-                    .where(consts.MATCH_TABLE.c.matchId == matchId)
-                    .values(ended=True, length=length)
-                )
-            await conn.commit()
+        conn = await self.conn()
+        for matchId, length in args:
+            await conn.execute(
+                update(consts.MATCH_TABLE) \
+                .where(consts.MATCH_TABLE.c.matchId == matchId)
+                .values(ended=True, length=length)
+            )
+        await conn.commit()
 
     async def insert_new_players(self, data: List[Dict[str, Any]]):
-        async with self.engine.connect() as conn:
-            for row in data:
-                try:
-                    row['result'] = None
-                    await conn.execute(insert(consts.MATCH_PLAYER_TABLE).values(**row))
-                except IntegrityError:
-                    await conn.rollback()
-                else:
-                    await conn.commit()
+        conn = await self.conn()
+        for row in data:
+            try:
+                row['result'] = None
+                await conn.execute(insert(consts.MATCH_PLAYER_TABLE).values(**row))
+            except IntegrityError:
+                await conn.rollback()
+            else:
+                await conn.commit()
 
     async def update_players(self, data: List[Dict[str, Any]]):
-        async with self.engine.connect() as conn:
-            await conn.execute(
-                update(consts.MATCH_PLAYER_TABLE) \
-                .where(and_(
-                    consts.MATCH_PLAYER_TABLE.c.matchId == bindparam('_matchId'),
-                    consts.MATCH_PLAYER_TABLE.c.profileId == bindparam('_profileId'),
-                ))
-                .values(result=bindparam('_result')),
-                data
-            )
-            await conn.commit()
+        conn = await self.conn()
+        await conn.execute(
+            update(consts.MATCH_PLAYER_TABLE) \
+            .where(and_(
+                consts.MATCH_PLAYER_TABLE.c.matchId == bindparam('_matchId'),
+                consts.MATCH_PLAYER_TABLE.c.profileId == bindparam('_profileId'),
+            ))
+            .values(result=bindparam('_result')),
+            data
+        )
+        await conn.commit()
 
 
     async def query_finished(self) -> List[int]:
-        async with self.engine.connect() as conn:
-            result = (await conn.execute(
-                select(consts.FINISHED_MATCH_TABLE.c.matchId).limit(30)
-            )).all()
-            return [_[0] for _ in result]
+        conn = await self.conn()
+        result = (await conn.execute(
+            select(consts.FINISHED_MATCH_TABLE.c.matchId).limit(30)
+        )).all()
+        return [_[0] for _ in result]
 
     async def query_players(self, *matchIds: int) -> List[Tuple[int, int]]:
-        async with self.engine.connect() as conn:
-            result = (await conn.execute(
-                select(
-                    consts.MATCH_PLAYER_TABLE.c.matchId.distinct(),
-                    consts.MATCH_PLAYER_TABLE.c.profileId
-                )
-                .where(consts.MATCH_PLAYER_TABLE.c.matchId.in_(matchIds))
-            )).all()
-            return result
+        conn = await self.conn()
+        result = (await conn.execute(
+            select(
+                consts.MATCH_PLAYER_TABLE.c.matchId.distinct(),
+                consts.MATCH_PLAYER_TABLE.c.profileId
+            )
+            .where(consts.MATCH_PLAYER_TABLE.c.matchId.in_(matchIds))
+        )).all()
+        return result
 
     async def query_ongoing_match(self):
-        async with self.engine.connect() as conn:
-            result = (await conn.execute(
-                select(consts.ONGOING_MATCH_TABLE.c.matchId).limit(2000)
-            )).all()
-            return {_[0] for _ in result}
+        conn = await self.conn()
+        result = (await conn.execute(
+            select(consts.ONGOING_MATCH_TABLE.c.matchId).limit(2000)
+        )).all()
+        return {_[0] for _ in result}
 
     async def close(self):
+        if self._conn is not None:
+            await self._conn.close()
         await self.engine.dispose()
